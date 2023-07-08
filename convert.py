598cc1a87cd0d05d6c8c844bfff8d7702c36f03f
Nived Padikkal <97820044+nivutechy@users.noreply.github.com>
2023-07-08 05:37:34 +0530

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,18 +1,13 @@
 import bpy
 
-# Set the path to your OSM file
+# Set the path to the OSM file
 osm_file = "map.osm"
 
-# Set the path to output the OBJ file
-obj_file = "output.obj"
+# Import the OSM file using the OpenStreetMap addon
+bpy.ops.import_scene.openstreetmap(filepath=osm_file)
 
-# Import the OSM file
-bpy.ops.import_scene.osm(filepath=osm_file)
-
-# Set up any additional configuration or modifications
+# Set the output file path
+output_file = "map.obj"
 
 # Export the scene as OBJ
-bpy.ops.export_scene.obj(filepath=obj_file)
-
-# Quit Blender
-bpy.ops.wm.quit_blender()
+bpy.ops.export_scene.obj(filepath=output_file, use_selection=False, use_materials=False)
